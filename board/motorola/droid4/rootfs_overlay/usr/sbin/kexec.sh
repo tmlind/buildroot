1b7b0449866a23fa197fe6acc2c7a41d39713f96
Tony Lindgren <tony@atomide.com>
2017-12-17 14:21:24 -0800

    Really fix booting v3.0.8 kernels from kexecboot
    We must rewrite the arguments to the legacy kexec to use naming
    --devtree instead of --dtb. And it also needs --atags file passed,
    let's try to find the atags file in the same directory as the dtb.
    
    We can test for the legacy little endian dtb format as it uses "edfe0dd0"
    instead of "d00dfeed" and rewrite the arguments if a legacy dtb is found.
    
    Later on we may be able to add these changes to the mainline kexec-tools,
    or at least carry a patch for it. But there are now dtb checks done in
    the mainline kexec-tools so somebody has to spend some time to get it
    patched. For now, let's just try to get legacy kernels booting first.
    
    Tested with v3.0.8 kernel with something like the following in the
    /boot/boot.cfg file for utagboot on mmcblk0p1 using some earlier random
    lineageOS snapshot I had around:
    
    LABEL=lineage-14.1-20171005-nightly-maserati
    PRIORITY=5
    DTB=/boot/lineage-14.1-20171005-nightly-maserati/devtree
    KERNEL=/boot/lineage-14.1-20171005-nightly-maserati/zImage
    INITRD=/boot/lineage-14.1-20171005-nightly-maserati/initrd.img
    CMDLINE=root=/dev/ram0 rw mem=1023M@0x80000000 console=null vram=10300K \
    omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:\
    p7(pds),p8(utags),p14(boot),p15(recovery),p16(cdrom),p17(misc),p18(cid),\
    p19(kpanic),p20(system),p21(cache),p22(preinstall),p23(webtop),\
    p24(userdata),p25(emstorage) mot_sst=1 androidboot.bootloader=0x0A74 \
    androidboot.hardware=mapphone_cdma androidboot.selinux=enforcing \
    buildvariant=userdebug
    TIMEOUT=3
    
    And I have the atags file copied into the same directory with the dtb
    at /boot/lineage-14.1-20171005-nightly-maserati.
    
    The 3.0.8 kernel starts booting but shuts down shortly after as the
    problem of legacy kernel initramfs still remains. The initramfs for
    the legacy kernels needs to be updated to use kernel cmdline instead
    of SafeStrap configuration on /dev/mmcblk1p25. Note that kexec just
    might work if you still have mmcblk1p25 formatted as vfat and SafeStrap
    installed.

--- board/motorola/droid4/rootfs_overlay/usr/sbin/kexec.sh
+++ board/motorola/droid4/rootfs_overlay/usr/sbin/kexec.sh
@@ -3,15 +3,74 @@
 # Wrapper script for kexec and kexec-droid4 as we need to use kexec-droid4
 # to boot v3.0.8 based kernels if non-standard kexec option "devtree" is set.
 #
-for var in "$@"; do
-	if echo "${var}" | grep devtree; then
+
+# Does the dtb use "edfe0dd0" instead of "d00df33d"?
+check_legacy_dtb() {
+	dtb=""
+
+	for var in "$@"; do
+		if echo "${var}" | grep "\-l" > /dev/null ||
+			echo "${var}" | grep "\--load" > /dev/null; then
+			loading=1
+			break
+		fi
+	done
+
+	if [ "${loading}" != 1 ]; then
+		return
+	fi
+
+	for var in "$@"; do
+		if echo "${var}" | grep "\--dtb=" > /dev/null; then
+			dtb=$(echo "${var}" | cut -d'=' -f2)
+			break
+		fi
+	done
+
+	if [ ! -f "${dtb}" ]; then
+		echo "Could not find dtb"
+		exit 1
+	fi
+
+	if hexdump -C ${dtb} | head -n1 | grep "ed fe 0d d0" > /dev/null; then
+		echo "Found legacy dtb.."
 		legacy=1
 		break
 	fi
-done
+}
+
+# Rewrite args for legacy kexec to use --devtree and --atags and assume
+# there is an atags file in the same directory as the dtb
+run_legacy_kexec() {
+	for var in "$@"; do
+		shift
+		if echo "${var}" | grep "\--dtb=" > /dev/null; then
+			legacy_arg=$(echo "${var}" | sed -e 's/--dtb/--devtree/;')
+			atag_path=$(dirname $(echo "${var}" | cut -d'=' -f2))
+			atag_path="${atag_path}/atags"
+			if [ ! -f ${atag_path} ]; then
+				echo "No atags file found at ${atag_path}"
+				return 1
+			fi
+		else
+			set -- "$@" "$var"
+		fi
+	done
+
+	if [ "${atag_path}" != "" ]; then
+		set - "${legacy_arg}" "$@"
+		set - "--atags=${atag_path}" "$@"
+	fi
+
+	echo "Starting legacy kexec with: "$@""
+	/usr/sbin/kexec-droid4 "$@"
+}
+
+check_legacy_dtb "$@"
 
 if [ "${legacy}" == "1" ]; then
-	/usr/sbin/kexec-droid4 "$@"
+	echo "Using legacy kexec.."
+	run_legacy_kexec "$@"
 else
 	/usr/sbin/kexec-mainline "$@"
 fi
