tmlind/buildroot

commit f449ce30a9e433d91767d5d50afc1e7646e72e58

Add initial battery charging support with battd
Let's start stock Android battd using runbattd wrapper before we start
kexecboot. This allows battery to charge while kexecboot is running.
When stopping kexecboot, we stop battd, and if exited from kexecboot we
start battd again.

For now, to keep kexecboot running to charge, you can select System menu
-> Show debug info. There is currently no charge status support.

We can add a small app to show the charge status, or integrate support
into kexecboot. Note that space is a concern here as we need to fit
everything into the 4 MiB boot image.

If using a serial console to debug the bootloader, note that serial port
will not currently work after plugging in a USB charger.

Signed-off-by: Tony Lindgren <tony@atomide.com>

board/motorola/droid4/rootfs_overlay/sbin/preinit.sh
@@ -33,7 +33,11 @@ init_system() {
 		mount -t proc none /proc
 		mount -t sysfs none /sys
 		mount -t tmpfs none /dev
+		mount -t tmpfs none /run
+		mount -t tmpfs none /tmp
 		mdev -s
+		mkdir /dev/pts
+		mount -tdevpts - /dev/pts
 	fi
 
         kernel_version=$(uname -r)
@@ -180,6 +184,10 @@ init_system
 load_modules
 set_root_passwd
 
+# Let's run battd while kexecboot is running. Note that it takes some time
+# after starting battd to show right battery voltages.
+/etc/init.d/S30battd start
+
 if [ "${start_kexecboot}" == 1 ]; then
 	/usr/bin/kexecboot
 fi
@@ -188,9 +196,15 @@ if [ "$$" != "1" ]; then
 	exit 0
 fi
 
+# Kexecboot stopped, stop battd. It gets started again as a normal start-up
+# script if pivot_root was not requested.
+/etc/init.d/S30battd stop
+
 if [ ! -f /tmp/pivot_root ]; then
 	echo "Continue normal init.."
+	umount /run
 	umount /tmp
+	umount /dev/pts
 	umount /dev
 	umount /sys
 	umount /proc

board/motorola/droid4/post-build.sh
@@ -12,6 +12,44 @@ if [ ! -d ${TARGET_DIR}/lib/firmware ]; then
 	mkdir -p ${TARGET_DIR}/lib/firmware
 fi
 
+# Prepare things for running Android battd. These get mounted by the
+# battd start-up script as the partition layout depends on the
+# device and we don't have /dev/disk/by-id available.
+if [ ! -d ${TARGET_DIR}/system ]; then
+	mkdir -p ${TARGET_DIR}/system
+fi
+
+if [ ! -d ${TARGET_DIR}/pds ]; then
+	mkdir -p ${TARGET_DIR}/pds
+fi
+
+if [ ! -d ${TARGET_DIR}/data ]; then
+	mkdir -p ${TARGET_DIR}/data
+fi
+
+if [ ! -d ${TARGET_DIR}/acct ]; then
+	mkdir -p ${TARGET_DIR}/acct
+fi
+
+# Battd changes the user to 9000 on startup
+if grep battd output/target/etc/passwd; then
+	sed -i '/.*battd.*/d' output/target/etc/passwd
+fi
+
+if grep battd output/target/etc/group; then
+	sed -i '/.*battd.*/d' output/target/etc/group
+fi
+
+echo "battd:x:9000:9000:battd,,,:/home/battd:/bin/false" >> output/target/etc/passwd
+echo "battd:x:9000:" >> output/target/etc/group
+
+# Make some room for runbattd by removing some apps
+rm -f output/target/sbin/e2freefrag
+rm -f output/target/sbin/e4crypt
+rm -f output/target/sbin/logsave
+rm -f output/target/usr/sbin/kdump
+rm -f output/target/usr/sbin/vmcore-dmesg
+
 #
 # Only enable networking if user creates /etc/wpa_supplicant.conf.
 #

board/motorola/droid4/rootfs_overlay/etc/init.d/S30battd
@@ -1,0 +1,127 @@
+#!/bin/sh
+
+get_emmc_partition_by_name() {
+	name="${1}"
+	part=$(cat /proc/cmdline | grep -o "p[0-9]*(${name})" | cut -d'(' -f1)
+	echo ${part}
+}
+
+mount_emmc_partition_by_name() {
+	name="${1}"
+	mountpoint="${2}"
+	options="${3}"
+
+	part=$(get_emmc_partition_by_name ${name})
+	if [ "${part}" == "" ]; then
+		echo "Could not get partition for ${name}"
+		return
+	fi
+
+	mount -o ${options} /dev/mmcblk1${part} ${mountpoint}
+}
+
+prepare_stock_partitions() {
+	if ! mount | grep "on /system"; then
+		mount_emmc_partition_by_name system /system ro
+	fi
+
+	if ! mount | grep "on /pds"; then
+		mount_emmc_partition_by_name pds /pds rw
+	fi
+
+	if ! mount | grep "on /acct"; then
+		mount -t tmpfs tmpfs /acct
+		mkdir /acct/uid
+	fi
+
+	# Copy battd files from userdata, otherwise kexecboot won't be able
+	# to use the data partition for kernels to load. We copy them back on
+	# when stopping battd.
+	if ! mount | grep "on /data"; then
+		mount -t tmpfs tmpfs /data
+
+		mount_emmc_partition_by_name userdata /mnt ro
+		if [ -d /mnt/battd ]; then
+			cp -r /mnt/battd /data/
+		else
+			mkdir /data/battd
+		fi
+
+		umount /mnt
+	fi
+
+	# Remove log socket if it exists, Android wants a directory..
+	if [ -S /dev/log ]; then
+		rm -f /dev/log
+	fi
+
+	if [ ! -d /dev/log ]; then
+		mkdir /dev/log
+		ln -s /dev/log_main /dev/log/main
+		ln -s /dev/log_radio /dev/log/radio
+		ln -s /dev/log_events /dev/log/events
+		ln -s /dev/log_system /dev/log/system
+	fi
+}
+
+cleanup() {
+	killall battd
+
+	# Copy back battd files to userdata
+	part=$(get_emmc_partition_by_name userdata)
+
+	if mount | grep "on /data"; then
+		# userdata may be mounted by kexecboot to boot from
+		if mount | grep /dev/mmcblk1${part}; then
+			echo "userdata already mounted by kexecboot"
+			mount -o rw,remount /dev/mmcblk1${part}
+			cp -r /data/battd/* /mnt/battd/
+			mount -o ro,remount /dev/mmcblk1${part}
+		else
+			echo "userdata not mounted by kexecboot"
+			mountpoint=$(mktemp -d)
+
+			if [ "${mountpoint}" != "" ]; then
+				mount_emmc_partition_by_name userdata ${mountpoint} rw
+
+				if [ -d ${mountpoint}/battd ]; then
+					cp /data/battd/* ${mountpoint}/battd/
+				fi
+				umount ${mountpoint}
+			fi
+		fi
+	fi
+
+	umount /data
+	umount /acct
+	umount /pds
+	umount /system
+}
+
+case "$1" in
+  start)
+	echo "Starting stock kernel battd.."
+	prepare_stock_partitions
+
+	# Start battd with runbattd wrapper
+	runbattd &
+
+	# We get a bogus battery state for a few seconds on boot
+	for i in $(seq 5); do
+		state=$(cat /sys/class/power_supply/battery/status)
+		if [ "${state}" != "Discharging" ]; then
+			break
+		fi
+		sleep 1
+	done
+
+	;;
+  stop)
+	cleanup
+	;;
+  *)
+	echo "Usage: $0 {start|stop}"
+	exit 1
+esac
+
+exit $?

board/motorola/droid4/rootfs_overlay/usr/sbin/kexec.sh
@@ -138,6 +138,9 @@ run_legacy_kexec() {
 	/usr/sbin/kexec-droid4 "$@"
 }
 
+# Always stop battd before kexec and pivot_root
+/etc/init.d/S30battd stop
+
 check_pivot_root "$@"
 load_modules
 check_legacy_dtb "$@"

configs/droid4_kexecboot_defconfig
@@ -15,6 +15,7 @@ BR2_ROOTFS_OVERLAY="board/motorola/droid4/rootfs_overlay"
 BR2_ROOTFS_POST_BUILD_SCRIPT="board/motorola/droid4/post-build.sh"
 BR2_PACKAGE_BUSYBOX_WATCHDOG=y
 BR2_PACKAGE_BUSYBOX_WATCHDOG_PERIOD="10"
+BR2_PACKAGE_RUNBATTD=y
 BR2_PACKAGE_KEXEC=y
 BR2_PACKAGE_KEXEC_ZLIB=y
 BR2_PACKAGE_KEXEC_DROID4=y

commit d8c9a4f0fa2cc89f9622ecfee3fbf7da80dfe1ca

Fix pivot_root to stock android trashing the bootloader
If we fail to umount the bootloader rootfs after pivot_root to the
Android initramfs, bootloader won't work on the next boot as Android
seems to change the mounted bootloader executable permissions or
something like that.

Let's umount run and tmp so we can umount the old root after pivot_root.

Let's also change to use new_root instead of data name for the new root
to avoid confusion on what is mounted and why.

Fixes: f449ce30a9e4 ("Add initial battery charging support with battd")
Signed-off-by: Tony Lindgren <tony@atomide.com>

board/motorola/droid4/rootfs_overlay/sbin/preinit.sh
@@ -137,6 +137,9 @@ unpack_initramfs() {
 
 	rm -f /mnt/initrd
 
+	# For pivot_root
+	mkdir /mnt/old_root
+
 	return 0
 }
 
@@ -173,7 +176,7 @@ prepare_pivot_root() {
 	mount --move /sys /mnt/sys
 	mount --move /proc /mnt/proc
 
-	cd /mnt && pivot_root . data
+	cd /mnt && pivot_root . old_root
 }
 
 if [ -f /etc/preinit ]; then
@@ -211,6 +214,8 @@ if [ ! -f /tmp/pivot_root ]; then
 	exec /sbin/init
 else
 	echo "Attempting pivot_root to stock Android distro.."
+	umount /run
+	umount /tmp
 
 	# Clear LCD for SafeStrap logo
 	dd if=/dev/zero of=/dev/fb0
@@ -218,6 +223,6 @@ else
 	prepare_pivot_root
 	rmmod board_mapphone_emu_uart
 	exec /bin/busybox chroot . /bin/busybox sh -c \
-	     '/bin/busybox umount /data; exec /init $*' \
+	     '/bin/busybox umount /old_root; exec /init $*' \
 	     < /dev/console > /dev/console 2>&1
 fi